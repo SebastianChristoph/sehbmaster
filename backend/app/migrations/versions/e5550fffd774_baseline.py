781977d45b82989c7309231c6042f7837dceae70
SebastianChristoph <sebastianchristoph2@gmail.com>
2025-09-17 14:37:38 +0200

    add migration

---
+++ backend/app/migrations/versions/e5550fffd774_baseline.py
@@ -0,0 +1,16 @@
+# backend/app/migrations/versions/e5550fffd774_baseline.py
+from alembic import op
+import sqlalchemy as sa
+
+# Revision-IDs (EXAKT so lassen!)
+revision = 'e5550fffd774'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    # Baseline – macht absichtlich nichts
+    pass
+
+def downgrade():
+    pass
